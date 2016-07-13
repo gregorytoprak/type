0d7145793f8bc2f3f0cad2ee308b5cf8c19d0a02
gdtoprak <gdtoprak@users.noreply.github.com>
2016-07-13 18:01:03 -0400

    Include character tiers based on letter frequency, and some state refactoring

--- ttt.js
+++ ttt.js
@@ -1,61 +1,73 @@
-$(document).ready(setStage)
+$(document).ready(function() {
 
-$(document).on("keyup", update)
-
-var ladder = makeLadder()
-
-function makeLadder() {
-  var cards = "abcdefghijklmnopqrstuvwxyz;,./".split("")
-  var ladder = {}
+  var state = {}
+  state.tier = 0
+  state.ladder = {}
+  var cards = "etaoinsrhldcumfpgwybvkxjqz./,;".split("")
   for (var i in cards) {
     var card = cards[i]
-    ladder[card] = {step: 0, due: $.now()}
+    state.ladder[card] = {step: 0, tier: Math.floor(i/10), due: $.now()}
   }
-  return ladder
-}
 
-function update() {
-  var card = $("#card").text()
-  var input = $("#input").val()
-  if (input.length >= card.length) {
-    var highlightColor
-    if (input === card) {
-      ladder[card].step += 1
-      highlightColor = "#0a0"
-    } else {
-      ladder[card].step = 0
-      highlightColor = "#a00"
+  setStage()
+
+  $(document).on("keyup", function() {
+    var card = $("#card").text()
+    var input = $("#input").val()
+    if (input.length >= card.length) {
+      var highlightColor
+      if (input === card) {
+        state.ladder[card].step += 1
+        highlightColor = "#0a0"
+      } else {
+        state.ladder[card].step = 0
+        highlightColor = "#a00"
+      }
+      state.ladder[card].due = $.now()+1000*delaySeconds(state.ladder[card].step)
+      $("#highlight")
+        .css("background-color", highlightColor)
+        .fadeIn(0, function() { $("#highlight").fadeOut() })
+      setStage()
     }
-    ladder[card].due = $.now()+1000*delaySeconds(ladder[card].step)
-    $("#highlight")
-      .css("background-color", highlightColor)
-      .fadeIn(0, function() { $("#highlight").fadeOut() })
-    setStage()
+  })
+
+  function setStage() {
+    $("#card").text(pickCard())
+    $("#input").val("")
   }
-}
 
-function setStage() {
-  $("#card").text(pickCard(ladder))
-  $("#input").val("")
-}
+  function delaySeconds(step) {
+    return Math.pow(2, step)
+  }
 
-function delaySeconds(step) {
-  return Math.pow(2, step)
-}
-
-function pickCard(ladder) {
-  var dueCards = []
-  for (var card in ladder) {
-    if (ladder[card].due < $.now()) {
-      dueCards.push(card)
+  function updateTier() {
+    var grow = true
+    for (var card in state.ladder) {
+      if (state.ladder[card].tier <= state.tier && state.ladder[card].step < 5) {
+        grow = false
+      }
+    }
+    if (grow) {
+      state.tier += 1
     }
   }
-  if (dueCards.length === 0) {
-    return "WIN"
-  }
-  return randItem(dueCards)
-}
 
-function randItem(arr) {
-  return arr[Math.floor(Math.random() * arr.length)];
-}
+  function pickCard() {
+    updateTier()
+    var dueCards = []
+    for (var card in state.ladder) {
+      if (state.ladder[card].tier <= state.tier && state.ladder[card].due  <= $.now()) {
+        dueCards.push(card)
+      }
+    }
+    if (dueCards.length === 0) {
+      return "WIN"
+    }
+    return randItem(dueCards)
+  }
+
+  function randItem(arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
+  }
+
+})
