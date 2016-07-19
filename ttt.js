64c3f63caf096c6224783a8a883f6777273c0bc1
gdtoprak <gdtoprak@users.noreply.github.com>
2016-07-19 15:53:01 -0400

    Refactoring for clarity and simplicity

--- ttt.js
+++ ttt.js
@@ -1,87 +1,66 @@
 $(document).ready(function() {
 
-  var state = {}
-  state.tier = 0
-  state.ladder = {}
-  var downcase = "etaoinsrhldcumfpgwybvkxjqz./,;1234567890-][`'\\=".split("")
-  var upcase   = "ETAOINSRHLDCUMFPGWYBVKXJQZ>?<:!@#$%^&*()_}{~\"|+".split("")
-  var chars = [...downcase, ...upcase]
-  for (var i in chars) {
-    var char = chars[i]
-    state.ladder[char] = {step: 0, tier: Math.floor(i/10), due: $.now()}
-  }
-  state.ladder[" "] = {step: Infinity, tier: Infinity, due: Infinity}
+  var rawLadder = [
+    [ ["[space]"," "] ],
+    [ ["e"],["t"],["a"],["o"],["i"],["n"],["s"],["r"],["h"],["l"] ],
+    [ ["d"],["c"],["u"],["m"],["f"],["p"],["g"],["w"],["y"],["b"] ],
+    [ ["v"],["k"],["x"],["j"],["q"],["z"] ],
+    [ ["1"],["2"],["3"],["4"],["5"],["6"],["7"],["8"],["9"],["0"] ],
+    [ ["."],["/"],[","],[";"],["-"],["]"],["["],["`"],["'"],["\\"],["="] ],
+    [ ["E"],["T"],["A"],["O"],["I"],["N"],["S"],["R"],["H"],["L"] ],
+    [ ["D"],["C"],["U"],["M"],["F"],["P"],["G"],["W"],["Y"],["B"] ],
+    [ ["V"],["K"],["X"],["J"],["Q"],["Z"] ],
+    [ ["!"],["@"],["#"],["$"],["%"],["^"],["&"],["*"],["("],[")"] ],
+    [ [">"],["?"],["<"],[":"],["_"],["}"],["{"],["~"],["\""],["|"],["+"] ]
+  ]
 
-  state.init = true
-  $("#front").text("[space]")
+  var randomItem = (a) => a[Math.floor(Math.random()*a.length)]
+
+  class Card {
+    constructor(front, back = front) {
+      this.front = front
+      this.back = back
+      this.hits = 0
+      this.due = $.now()
+    }
+  }
+
+  class State {
+    constructor() {
+      this.ladder = rawLadder.map( (rawTier) => rawTier.map( (rawCard) => new Card(...rawCard) ) )
+      this.step = 0
+    }
+    transition() {
+      if (this.ladder[this.step].every( (card) => (card.hits >= 1) )) { this.step += 1 }
+
+      var dueCards = this.ladder[this.step].filter( (card) => (card.due <= $.now()) )
+      this.card = randomItem(dueCards)
+      $("#front").text(this.card.front)
+      $("#input").val("")
+    }
+  }
+
+  var state = new State()
+  state.transition()
 
   $(document).on("keyup", function() {
-    var front
-    if (state.init) {
-      front = " "
-    } else {
-      front = $("#front").text()
-    }
     var input = $("#input").val()
-    if (input.length >= front.length) {
+    if (input.length >= state.card.back.length) {
       var highlightColor
-      if (input === front) {
-        state.ladder[front].step += 1
+      if (input === state.card.back) {
+        state.card.hits += 1
         highlightColor = "#0a0"
       } else {
-        state.ladder[front].step = 0
+        state.card.hits = 0
         highlightColor = "#a00"
       }
-      if (state.init) {
-        state.init = false
-      } else {
-        state.ladder[front].due = $.now()+1000*delaySeconds(state.ladder[front].step)
-      }
+      state.card.due = $.now()+1000*Math.pow(2, state.card.hits)
       $("#highlight")
         .css("background-color", highlightColor)
         .fadeIn(0, function() { $("#highlight").fadeOut() })
-      setStage()
+
+      state.transition()
     }
   })
-
-  function setStage() {
-    $("#front").text(pickCard())
-    $("#input").val("")
-  }
-
-  function delaySeconds(step) {
-    return Math.pow(2, step)
-  }
-
-  function updateTier() {
-    var grow = true
-    for (var card in state.ladder) {
-      if (state.ladder[card].tier <= state.tier && state.ladder[card].step < 3) {
-        grow = false
-      }
-    }
-    if (grow) {
-      state.tier += 1
-    }
-  }
-
-  function pickCard() {
-    updateTier()
-    var dueCards = []
-    for (var card in state.ladder) {
-      if (state.ladder[card].tier <= state.tier && state.ladder[card].due  <= $.now()) {
-        dueCards.push(card)
-      }
-    }
-    if (dueCards.length === 0) {
-      state.init = true
-      return "[space]"
-    }
-    return randItem(dueCards)
-  }
-
-  function randItem(arr) {
-    return arr[Math.floor(Math.random() * arr.length)];
-  }
 
 })
