a8042da6bf89fbf7d6c57c0c832c15b5aa12e761
gdtoprak <gdtoprak@users.noreply.github.com>
2016-07-20 17:26:05 -0400

    Write readme, refigure tiers

--- README.md
+++ README.md
@@ -1,1 +1,59 @@
+# Touch Typing Trainer
+
+"This touch typing trainer teaches through the theory of spaced repetition."
+
 [ttt.gdt.io](http://ttt.gdt.io)
+
+## What it is
+
+Type the presented character without looking at your keyboard to learn its location and improve your typing.
+
+Designed for the early stages of learning to touch type. This trainer repeatedly gives you characters (and short n-grams) to type at an interval determined by your history with that character.
+
+This allows your muscle memory to develop; it teaches you to remember where each key is without having to look.
+
+After mastery of key locations has been developed, common bigrams and trigrams are introduced for greater typing efficacy.
+
+## How it works
+
+There are a number of 'tiers' of characters. You start in the first tier, with the ten [most common](http://norvig.com/mayzner.html) letters in English. You advance to the next tier once every character in the current tier has been typed correctly 3 times in a row. That is to say, once every character is in box 3 or above.
+
+In each tier, the [Leitner System](https://en.wikipedia.org/wiki/Leitner_system) of spaced repetition is used.
+
+Every character begins in box zero and is 'due'. When you successfully type a character, it advances one box. (Incorrectly typing a character sends it back to box zero.)
+
+After typing a character, its next appearance time is based on its box number: a character that has just entered box k will be next due in 2^k seconds.
+
+To choose a character to be typed, we select randomly from all of the characters currently due for the current tier. (If nothing is currently due, the character that will become due next is chosen.)
+
+That's all there is to it, except for the choice of characters in tiers.
+
+## Tiers
+
+1. top ten most common letters
+ - (e t a o i n s r h l)
+2. second ten most common letters
+ - (d c u m f p g w y b)
+3. 1 and 2 combined
+4. last six letters
+ - (v k x j q z)
+5. 1 and 4 combined
+6. 2 and 4 combined
+7. 1, 2, and 4 combined (the whole alphabet)
+8. the whole alphabet capitalized
+9. the ten digits
+ - (1 2 3 4 5 6 7 8 9 0)
+10. the thirty-two standard punctuation characters on the keyboard/in ASCII
+ - (! @ # $ % ^ & * ( ) [ ] \` \ ' = / ; , . - { } ~ | " + ? : < > \_)
+11. top ten most common digraphs
+ - (th he in er an re on at en nd)
+12. top ten most common trigraphs
+ - (the and ing ion tio ent ati for her ter)
+
+## Limitations
+
+This trainer is not enough for absolute mastery, of course. After sufficient skill has been developed here, the final step is to use this touch typing in your everyday keyboard use! It is only over time that true skill can be developed.
+
+I hope touch typing becomes so natural that you forget it was even a skill you had to learn.
+
+Good luck!
--- ttt.js
+++ ttt.js
@@ -1,39 +1,43 @@
 $(document).ready(function() {
 
-  var rawLadder = [
-    "e t a o i n s r h l",
-    "d c u m f p g w y b",
-    "v k x j q z",
-    "th he in er an re on at en nd",
-    "the and ing ion tio ent ati for her ter",
-    "tion atio that ther with ment ions this here from",
-    "1 2 3 4 5 6 7 8 9 0",
-    "; / , . - [ ] \\ ' ` =",
-    "E T A O I N S R H L",
-    "D C U M F P G W Y B",
-    "V K X J Q Z",
-    "! @ # $ % ^ & * ( )",
-    ": ? < > _ { } | \" ~ +"
-  ]
+var rawLadder =
+`e t a o i n s r h l
+d c u m f p g w y b
+e t a o i n s r h l d c u m f p g w y b
+v k x j q z
+e t a o i n s r h l v k x j q z
+d c u m f p g w y b v k x j q z
+e t a o i n s r h l d c u m f p g w y b v k x j q z
+E T A O I N S R H L D C U M F P G W Y B V K X J Q Z
+1 2 3 4 5 6 7 8 9 0
+! @ # $ % ^ & * ( ) [ ] \` \\ \' = / ; , . - { } ~ | \" + ? : < > _
+th he in er an re on at en nd
+the and ing ion tio ent ati for her ter`.split("\n").map( (line) => line.split(" ") )
+
+  var color = {red: "#e74c3c", green: "#2ecc71", blue: "#3498db"} // https://flatuicolors.com/
 
   function randomItem(array) {
     return array[ Math.floor(array.length * Math.random()) ]
+  }
+
+  function dueTime(box) {
+    return $.now()+1000*Math.pow(2, box)
   }
 
   class Card {
     constructor(face, back = face) {
       this.face = face
       this.back = back
-      this.hits = 0
+      this.box = 0
       this.due = $.now()
     }
   }
 
   class State {
     constructor() {
-      this.ladder = rawLadder.map( (tierString) => tierString.split(" ").map( (cardFace) => new Card(cardFace) ) )
+      this.ladder = rawLadder.map( (rawTier) => rawTier.map( (cardFace) => new Card(cardFace) ) )
       this.step = 0
-      this.highlightColor = "#00a"
+      this.highlightColor = color.blue
       this.transition()
     }
     keyup() {
@@ -42,24 +46,29 @@ $(document).ready(function() {
         return
       }
       if (this.input === this.card.back) {
-        this.card.hits += 1
-        this.highlightColor = "#0a0"
+        this.card.box += 1
+        this.highlightColor = color.green
       } else {
-        this.card.hits = 0
-        this.highlightColor = "#a00"
+        this.card.box = 0
+        this.highlightColor = color.red
       }
-      this.card.due = $.now()+1000*Math.pow(2, this.card.hits)
+      this.card.due = dueTime(this.card.box)
 
       this.transition()
     }
     transition() {
+      if (this.ladder[this.step].every( (card) => card.box >= 3 )) {
+        this.ladder[this.step].forEach( (card) => {card.box = 0} )
+        this.step += 1
+        if (this.step >= this.ladder.length) {
+          this.step = 0
+          this.highlightColor = color.blue
+        }
+      }
+
       $("#highlight")
         .css("background-color", this.highlightColor)
         .fadeIn(0, function() { $("#highlight").fadeOut() })
-
-      if (this.ladder[this.step].every( (card) => card.hits >= 1 )) {
-        this.step += 1
-      }
 
       this.card = randomItem( this.ladder[this.step].filter( (card) => card.due <= $.now() ) )
       if (this.card === undefined) {
